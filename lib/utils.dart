24c94e2fbb792b8dbeaa72acd6d22a54005b1566
Varun Pujari <varunpvp@gmail.com>
2021-11-17 21:09:04 +0530

    add isPromoting util function

--- lib/utils.dart
+++ lib/utils.dart
@@ -22,4 +22,27 @@ PieceMap getPieceMap(String fen) {
   return map;
 }
 
+bool isPromoting(String fen, ShortMove move) {
+  final chess = ch.Chess.fromFEN(fen);
+
+  final piece = chess.get(move.from);
+
+  if (piece?.type != ch.PieceType.PAWN) {
+    return false;
+  }
+
+  if (piece?.color != chess.turn) {
+    return false;
+  }
+
+  if (!["1", "8"].any((it) => move.to.endsWith(it))) {
+    return false;
+  }
+
+  return chess
+      .moves({"square": move.from, "verbose": true})
+      .map((it) => it["to"])
+      .contains(move.to);
+}
+
 noop1(arg1) {}
---
+++ test/utils_test.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_stateless_chessboard/flutter_stateless_chessboard.dart';
+import 'package:flutter_stateless_chessboard/utils.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group("isPromoting", () {
+    final fen = "rnbqkbnr/pP2pppp/8/8/8/8/PPPP1PPP/RNBQKBNR w KQkq - 1 5";
+
+    test("isPromoting returns true if promoting", () {
+      expect(
+        isPromoting(
+          fen,
+          ShortMove(from: "b7", to: "a8", promotion: PieceType.QUEEN),
+        ),
+        isTrue,
+      );
+    });
+
+    test("isPromoting returns false not if promoting", () {
+      expect(
+        isPromoting(
+            fen, ShortMove(from: "a2", to: "a3", promotion: PieceType.QUEEN)),
+        isFalse,
+      );
+    });
+  });
+}
